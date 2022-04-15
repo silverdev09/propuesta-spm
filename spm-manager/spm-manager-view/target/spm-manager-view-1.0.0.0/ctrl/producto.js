f2e4657205855934090322deda9d5e4ac5c053af
Silverio Paco Machaca <silverpaco09@gmail.com>
2022-04-14 23:53:11 -0400

    Se crea el proyecto

---
+++ spm-manager/spm-manager-view/target/spm-manager-view-1.0.0.0/ctrl/producto.js
@@ -0,0 +1,81 @@
+
+__app.controller("spm$producto", function ($scope, $http) {
+    const PATH = "manager/spm";
+    $scope.view = new ViewHandler(PATH, 'producto', ['inbox','crear', 'editar', 'borrar']);
+    $scope.part = new PartHandler(PATH, 'producto');
+    var productoServ = new ServHandler(PATH, 'producto', ['listar', 'crear', 'editar', 'borrar']);
+    var productoPanel = $scope.panel = new PanelHandler();
+    var productoData = $scope.data = {
+        list: [],
+        current: undefined
+    };
+    var productoFilter = $scope.filter = {
+        apply: function () {
+            var call = $http.post(productoServ.listar, {});
+            call.success(function (data) {
+                productoData.list = data;
+            });
+            call.error(function () {
+                productoData.list = [];
+            });
+        },
+        clear: function () {
+            productoData.list = [];
+        }
+    };
+    productoFilter.apply();
+    $scope.table = {
+        toggle: function (value) {
+            if (productoData.current && productoData.current.idProducto === value.idProducto) {
+                productoData.current = null;
+            } else {
+                productoData.current = angular.copy(value, {});
+            }
+        },
+        in: function (value) {
+            return productoData.current && productoData.current.idProducto === value.idProducto;
+        },
+        isOneSelect: function () {
+            return productoData.current && true;
+        },
+        isMoreSelect: function () {
+            return productoData.current && true;
+        }
+    };
+    $scope.action = {
+        open: function (name) {
+            if (name === 'crear') {
+                productoData.current = {};
+            }
+            productoPanel.open(name);
+        },
+        cancel: function (name) {
+            productoData.current = undefined;
+            productoPanel.close(name);
+        },
+        create: function () {
+            var call = $http.post(productoServ.crear, productoData.current);
+            call.success(function () {
+                productoData.current = {};
+                productoFilter.apply();
+                productoPanel.close('crear');
+            });
+        },
+        update: function () {
+            var call = $http.post(productoServ.editar, productoData.current);
+            call.success(function () {
+                productoData.current = undefined;
+                productoFilter.apply();
+                productoPanel.close('editar');
+            });
+        },
+        remove: function () {
+            var call = $http.post(productoServ.borrar, productoData.current);
+            call.success(function () {
+                productoData.current = undefined;
+                productoFilter.apply();
+                productoPanel.close('borrar');
+            });
+        }
+    };
+});
